f6f1046cdc96d46ca5fc3751deebde75b3cbfa0e
PC-Ow <PC-Ow@PC-OWNER>
2024-05-09 03:56:07 +0900

    プロジェクト ファイルを追加します。

---
+++ 2024_05_09_Task1/2024_05_09_Task1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#define TASK1
+
+template <typename TYPE>
+TYPE triangleArea(TYPE base, TYPE height) {
+    return (base * height / 2);
+}
+
+int main()
+{
+#ifdef TASK1
+    printf("三角形の面積 %d\n", triangleArea(3, 10));
+    printf("三角形の面積 %g\n", triangleArea(2.5, 5.4));
+#else
+    printf("三角形の面積 %d\n", triangleArea<int>(3, 10));
+    printf("三角形の面積 %d\n", triangleArea<int>(2.5, 5.4));
+#endif // TASK1
+
+    return 0;
+}
